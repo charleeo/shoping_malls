0f3a1fef0937d36d5db01d3ea794e0bea2fd8828
charleeo <charleeotaru@gmail.com>
2021-05-24 15:28:50 +0100

    new changes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
-    <title> @yield('title','shopping-malls|')  {{ config('app.name', 'Laravel') }}  </title>
+    <title> @yield('title','cie|')  {{ config('app.name', 'Laravel') }}  </title>
     {{--  dropbox CDN  --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.0/min/dropzone.min.css"> 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.0/dropzone.js"></script> 
