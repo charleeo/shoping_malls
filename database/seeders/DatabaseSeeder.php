670f7da82945c5f389cdff7c3218e1d106bdf91e
charleeo <charleeotaru@gmail.com>
2021-05-30 22:17:15 +0100

    Creating sever seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            BusinessCategorySeeder::class,
-            // product_category_seeder::class
+            // BusinessCategorySeeder::class,
+            product_category_seeder::class
         ]);
     }
 }
