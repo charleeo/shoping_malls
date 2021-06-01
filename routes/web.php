99448cb48fb4c34791489f3ae2fe3ce93d6cb174
charleeo <charleeotaru@gmail.com>
2021-06-01 23:16:48 +0100

    my latest

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::post('/save-users',[RegisterController::class,'storeUsers']);
     Route::get('/businesses',[BusinessDomainController::class,'index'])->name('businesses');
 // Route::domain('{domain}.'.env('APP_DOMAIN'))->group(function(){
 // });
-Route::subdomain();
+// Route::subdomain();
 
 Route::domain('{domain}.'.env('APP_DOMAIN'))->group(function(){
     Route::get('/home',[BusinessDomainController::class,'show'])->name('home-page');
