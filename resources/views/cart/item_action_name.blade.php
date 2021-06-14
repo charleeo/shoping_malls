b947e75974cb04ab9d8a8c0f256052938667f762
charleeo <charleeotaru@gmail.com>
2021-06-14 06:18:23 +0100

    Shopping cart coding

--- resources/views/cart/item_action_name.blade.php
+++ resources/views/cart/item_action_name.blade.php
@@ -1,9 +1,9 @@
 <div class="{{$data['item_id']}}">
     <div class="item-name-action pt-3 ">
-        <div class="col-md-4 col-sm-5">
+        <div class="">
             <small>Name: <b>{{$data['item_name']}}</b> </small>
         </div>
-        <div class="col-md-4 col-sm-5 delete-data">
+        <div class=" delete-data">
             <form action="/cart/delete" method="POST">
                 <button class="delete_cart_data btn btn-default">Delete  <b><i class="fas fa-trash text-danger"></i></b> </button>
                 <input type="hidden" name="product_id" value="{{$data['item_id']}}" class="product_id_to_delete">
