f830602416f1df1ba27257455751cb6f5d2fa9fc
charleeo <charleeotaru@gmail.com>
2021-06-21 05:56:57 +0100

    redesined the view

--- resources/views/pages/other_features.blade.php
+++ resources/views/pages/other_features.blade.php
@@ -5,7 +5,7 @@
             <p class="mss-text">
                 We have made a point of duty to make your business thrive. This is why we have to always put and your business first by putting your in front of the world. The world will always to get the notice whatsoever your have to sell or offer.
             </p>
-            <p class="mission-t text-secondary">
+            <p class="mss-text">
                 So if you got a business, bring it here let's sell it and resell it and keep selling it.
             </p>
         </div>
