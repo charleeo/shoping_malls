f64b7d614efe6282f5dc02bda0d9417879edc134
charleeo <charleeotaru@gmail.com>
2021-06-26 00:52:02 +0100

    New design

--- database/migrations/2021_06_24_232703_create_customers_table.php
+++ database/migrations/2021_06_24_232703_create_customers_table.php
@@ -15,6 +15,13 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('customer_name');
+            $table->string('customer_email');
+            $table->string('customer_phone');
+            $table->mediumText('address_line_1');
+            $table->mediumText('address_line_2')->nullable();
+            $table->integer('order_id');
+            $table->enum('order_status',['0','1'])->comment('0 = pending order. 1 = received order');
             $table->timestamps();
         });
     }
